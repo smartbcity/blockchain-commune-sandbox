d42732358916a6e377e85555023b2afd7404b2fa
Adrien Poupard <adrien.poupard@gmail.com>
2019-05-14 16:57:57 +0200

    bclan-it: Add current used version

---
+++ README.md
@@ -0,0 +1,15 @@
+# Network configuration: bclan-it
+
+Network with a generatied crypto to reproduce test environement
+
+# Start
+
+```
+docker-compose -f docker-compose-it.yaml up -d
+```
+
+# Down
+
+```
+docker-compose -f docker-compose-it.yaml down
+```
---
+++ fabric/util/init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source /opt/blockchain-coop/env
+
+#Join network
+echo 'Join channel ${CHANNEL}'
+peer channel create -o ${ORDERER_ADDR} -c ${CHANNEL} -f /etc/hyperledger/config/${CHANNEL}.tx --tls --cafile ${ORDERER_CERT}
+peer channel join -b ${CHANNEL}.block
+
+#Install and instantiate ssm
+source /opt/civis-blockchain/ssm/env
+echo 'Install chaincode ssm'
+peer chaincode install /opt/civis-blockchain/ssm/ssm.pak
+echo 'Instantiate chaincode ssm'
+peer chaincode instantiate -o ${ORDERER_ADDR} --tls --cafile ${ORDERER_CERT} -C ${CHANNEL} -n ${CHAINCODE} -v ${VERSION} -c $(cat /opt/blockchain-coop/init.arg) -P "OR ('BlockchainLANCoopMSP.member')"
+sleep 5
+peer chaincode query -C ${CHANNEL} -n ${CHAINCODE} -c '{"Args":["list", "user"]}'
