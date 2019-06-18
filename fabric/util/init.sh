f1cba8e94b2815890cec82c43ea6a34bceaae387
Adrien Poupard <adrien.poupard@gmail.com>
2019-06-18 15:07:28 +0200

    chore(ci): Add script to deploy ex02

--- fabric/util/init.sh
+++ fabric/util/init.sh
@@ -14,4 +14,5 @@ peer chaincode install /opt/civis-blockchain/ssm/ssm.pak
 echo 'Instantiate chaincode ssm'
 peer chaincode instantiate -o ${ORDERER_ADDR} --tls --cafile ${ORDERER_CERT} -C ${CHANNEL} -n ${CHAINCODE} -v ${VERSION} -c $(cat /opt/blockchain-coop/util/init.arg) -P "OR ('BlockchainLANCoopMSP.member')"
 sleep 5
+echo 'Query chaincode ssm'
 peer chaincode query -C ${CHANNEL} -n ${CHAINCODE} -c '{"Args":["list", "user"]}'
---
+++ fabric/util/initEx02.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source /opt/blockchain-coop/util/env
+
+#Join network
+echo 'Join channel ${CHANNEL}'
+peer channel create -o ${ORDERER_ADDR} -c ${CHANNEL} -f /etc/hyperledger/config/${CHANNEL}.tx --tls --cafile ${ORDERER_CERT}
+peer channel join -b ${CHANNEL}.block
+
+#Install and instantiate ex02
+echo 'Install chaincode ex02'
+peer chaincode install -n ex02 -v 1.0 -p blockchain-coop/go/chaincode_example02/
+echo 'Instantiate chaincode ex02'
+peer chaincode instantiate -o ${ORDERER_ADDR} --tls --cafile ${ORDERER_CERT} -C ${CHANNEL} -n ex02 -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "OR ('BlockchainLANCoopMSP.member')"
+sleep 5
+echo 'Query chaincode ex02'
+peer chaincode query -C ${CHANNEL} -n ex02 -c '{"Args":["query","a"]}'
